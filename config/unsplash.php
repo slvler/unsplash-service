5c999717b2cf048b303188d477ca5542b400beae
slvler <56368718+slvler@users.noreply.github.com>
2023-12-20 11:53:35 +0300

    Update unsplash.php

--- config/unsplash.php
+++ config/unsplash.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'base_uri' => 'https://api.unsplash.com/',
-    'access_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    'secret_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    'base_uri' => env('UNSPLASH_BASE_URL'),
+    'access_key' => env('UNSPLASH_ACCESS_KEY'),
+    'secret_key' => env('UNSPLASH_SECRET_KEY')
 ];
